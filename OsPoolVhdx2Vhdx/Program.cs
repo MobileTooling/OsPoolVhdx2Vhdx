4bb16c5c97eb7c4896d8133d34c4b5ce3083ccfc
Gustave Monce <gustave.monce@outlook.com>
2025-12-23 13:15:02 +0100

    Updates

--- OsPoolVhdx2Vhdx/Program.cs
+++ OsPoolVhdx2Vhdx/Program.cs
@@ -1,5 +1,4 @@
 ﻿using DiscUtils;
-using DiscUtils.Partitions;
 using DiscUtils.Streams;
 using StorageSpace;
 
@@ -46,96 +45,156 @@ namespace OsPoolVhdx2Vhdx
                 virtualDisk = new DiscUtils.Vhdx.Disk(vhdx, FileAccess.Read);
             }
 
-            PartitionTable partitionTable = virtualDisk.Partitions;
+            int sectorSize = virtualDisk.Geometry!.Value.BytesPerSector;//4096;//virtualDisk.Geometry!.Value.BytesPerSector;
+
+            IEnumerable<GPT.GPT.Partition>? partitionTable = GetGPTPartitions(virtualDisk.Content, (uint)sectorSize);
 
             if (partitionTable != null)
             {
-                foreach (PartitionInfo partitionInfo in partitionTable.Partitions)
+                foreach (GPT.GPT.Partition partitionInfo in partitionTable)
                 {
-                    if (partitionInfo.GuidType == new Guid("E75CAF8F-F680-4CEE-AFA3-B001E56EFC2D"))
+                    Stream partitionStream = Open(partitionInfo, (uint)sectorSize, virtualDisk.Content);
+
+                    if (partitionInfo.PartitionTypeGuid == new Guid("E75CAF8F-F680-4CEE-AFA3-B001E56EFC2D"))
                     {
-                        Logging.Log($"{((GuidPartitionInfo)partitionInfo).Name} {((GuidPartitionInfo)partitionInfo).Identity} {((GuidPartitionInfo)partitionInfo).GuidType} {((GuidPartitionInfo)partitionInfo).SectorCount * virtualDisk.SectorSize} StoragePool");
+                        Logging.Log();
 
-                        Stream storageSpacePartitionStream = partitionInfo.Open();
+                        Logging.Log($"{partitionInfo.Name} {partitionInfo.PartitionGuid} {partitionInfo.PartitionTypeGuid} {partitionInfo.SizeInSectors * (uint)sectorSize} StoragePool");
 
-                        Pool storageSpace = new(storageSpacePartitionStream);
+                        Pool pool = new(partitionStream);
 
-                        Dictionary<long, string> disks = storageSpace.GetDisks();
+                        Dictionary<long, string> disks = pool.GetDisks();
 
-                        foreach (KeyValuePair<long, string> disk in disks.OrderBy(x => x.Key).Skip(1))
+                        foreach (KeyValuePair<long, string> disk in disks.OrderBy(x => x.Key))
                         {
-                            using Space space = storageSpace.OpenDisk(disk.Key);
+                            using Space space = pool.OpenDisk(disk.Key);
 
                             Logging.Log($"- {disk.Key}: {disk.Value} ({space.Length}B / {space.Length / 1024 / 1024}MB / {space.Length / 1024 / 1024 / 1024}GB) StorageSpace");
                         }
 
-                        foreach (KeyValuePair<long, string> disk in disks.OrderBy(x => x.Key).Skip(1))
+                        Logging.Log();
+
+                        foreach (KeyValuePair<long, string> disk in disks)
                         {
-                            using Space space = storageSpace.OpenDisk(disk.Key);
+                            Space space = pool.OpenDisk(disk.Key);
 
-                            // Default is 4096
-                            int sectorSize = 4096;
+                            int spaceSectorSize = TryDetectSectorSize(space);
 
-                            if (space.Length > 4096 * 2)
-                            {
-                                BinaryReader reader = new(space);
-
-                                space.Seek(512, SeekOrigin.Begin);
-                                byte[] header1 = reader.ReadBytes(8);
-
-                                space.Seek(4096, SeekOrigin.Begin);
-                                byte[] header2 = reader.ReadBytes(8);
-
-                                string header1str = System.Text.Encoding.ASCII.GetString(header1);
-                                string header2str = System.Text.Encoding.ASCII.GetString(header2);
-
-                                if (header1str == "EFI PART")
-                                {
-                                    sectorSize = 512;
-                                }
-                                else if (header2str == "EFI PART")
-                                {
-                                    sectorSize = 4096;
-                                }
-                                else if (space.Length % 512 == 0 && space.Length % 4096 != 0)
-                                {
-                                    sectorSize = 512;
-                                }
-
-                                space.Seek(0, SeekOrigin.Begin);
-                            }
-                            else
-                            {
-                                if (space.Length % 512 == 0 && space.Length % 4096 != 0)
-                                {
-                                    sectorSize = 512;
-                                }
-                            }
-
-                            Logging.Log();
-
-                            string vhdFile = Path.Combine(outputDirectory, $"{disk.Value}.vhdx");
-                            Logging.Log($"Dumping {vhdFile}...");
-
-                            long diskCapacity = space.Length;
-                            using Stream fs = new FileStream(vhdFile, FileMode.CreateNew, FileAccess.ReadWrite);
-                            using VirtualDisk outDisk = DiscUtils.Vhdx.Disk.InitializeDynamic(fs, Ownership.None, diskCapacity, Geometry.FromCapacity(diskCapacity, sectorSize));
-
-                            DateTime now = DateTime.Now;
-                            Action<ulong, ulong> progressCallback = (ulong readBytes, ulong totalBytes) =>
-                            {
-                                ShowProgress(readBytes, totalBytes, now);
-                            };
-
-                            Logging.Log($"Dumping {disk.Value}");
-                            space.CopyTo(outDisk.Content, progressCallback);
-                            Logging.Log();
+                            DumpSpace(outputDirectory, disk.Value, space, spaceSectorSize);
 
                             space.Dispose();
                         }
                     }
                 }
             }
+        }
+
+        private static void DumpSpace(string outputDirectory, string disk, Space space, int spaceSectorSize)
+        {
+            Logging.Log();
+
+            string vhdFile = Path.Combine(outputDirectory, $"{disk}.vhdx");
+            Logging.Log($"Dumping {vhdFile}...");
+
+            long diskCapacity = space.Length;
+            using Stream fs = new FileStream(vhdFile, FileMode.CreateNew, FileAccess.ReadWrite);
+            using VirtualDisk outDisk = DiscUtils.Vhdx.Disk.InitializeDynamic(fs, Ownership.None, diskCapacity, Geometry.FromCapacity(diskCapacity, spaceSectorSize));
+
+            DateTime now = DateTime.Now;
+            void progressCallback(ulong readBytes, ulong totalBytes)
+            {
+                ShowProgress(readBytes, totalBytes, now);
+            }
+
+            Logging.Log($"Dumping {disk}");
+            space.CopyTo(outDisk.Content, progressCallback);
+            Logging.Log();
+        }
+
+        private static SubStream Open(GPT.GPT.Partition entry, uint SectorSize, Stream _diskData)
+        {
+            ulong start = entry.FirstSector * SectorSize;
+            ulong end = (entry.LastSector + 1) * SectorSize;
+
+            if ((long)end >= _diskData.Length)
+            {
+                end = (ulong)_diskData.Length;
+            }
+
+            return new SubStream(_diskData, (long)start, (long)(end - start));
+        }
+
+        private static List<GPT.GPT.Partition>? GetGPTPartitions(Stream diskStream, uint sectorSize)
+        {
+            diskStream.Seek(0, SeekOrigin.Begin);
+
+            try
+            {
+                byte[] buffer = new byte[sectorSize * 2];
+                diskStream.Read(buffer, 0, buffer.Length);
+                diskStream.Seek(0, SeekOrigin.Begin);
+
+                uint GPTBufferSize = OsPoolVhdx2Vhdx.GPT.GPT.GetGPTSize(buffer, sectorSize);
+
+                buffer = new byte[GPTBufferSize];
+                diskStream.Read(buffer, 0, buffer.Length);
+                diskStream.Seek(0, SeekOrigin.Begin);
+
+                GPT.GPT GPT = new(buffer, sectorSize);
+
+                return GPT.Partitions;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EXCEPTION!");
+                Console.WriteLine(ex);
+                diskStream.Seek(0, SeekOrigin.Begin);
+                return null;
+            }
+        }
+
+        private static int TryDetectSectorSize(Stream diskStream)
+        {
+            // Default is 4096
+            int sectorSize = 4096;
+
+            if (diskStream.Length > 4096 * 2)
+            {
+                BinaryReader reader = new(diskStream);
+
+                diskStream.Seek(512, SeekOrigin.Begin);
+                byte[] header1 = reader.ReadBytes(8);
+
+                diskStream.Seek(4096, SeekOrigin.Begin);
+                byte[] header2 = reader.ReadBytes(8);
+
+                string header1str = System.Text.Encoding.ASCII.GetString(header1);
+                string header2str = System.Text.Encoding.ASCII.GetString(header2);
+
+                if (header1str == "EFI PART")
+                {
+                    sectorSize = 512;
+                }
+                else if (header2str == "EFI PART")
+                {
+                    sectorSize = 4096;
+                }
+                else if (diskStream.Length % 512 == 0 && diskStream.Length % 4096 != 0)
+                {
+                    sectorSize = 512;
+                }
+
+                diskStream.Seek(0, SeekOrigin.Begin);
+            }
+            else
+            {
+                if (diskStream.Length % 512 == 0 && diskStream.Length % 4096 != 0)
+                {
+                    sectorSize = 512;
+                }
+            }
+
+            return sectorSize;
         }
 
         protected static void ShowProgress(ulong readBytes, ulong totalBytes, DateTime startTime)
